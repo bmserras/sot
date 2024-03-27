5ca74583e2c36dd91675af012b29d96a9f0e56d4
Bruno Serras <bruno.serras98.16@gmail.com>
2024-03-27 20:20:41 +0000

    Add dev-bundle

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-progress-bar-3b53bb70-57bc898a.js
@@ -0,0 +1,1 @@
+import{U as r}from"./indexhtml-a2ca16ca.js";const e={tagName:"vaadin-progress-bar",displayName:"ProgressBar",elements:[{selector:"vaadin-progress-bar::part(bar)",displayName:"Bar",properties:[r.backgroundColor]},{selector:"vaadin-progress-bar::part(value)",displayName:"Value",properties:[r.backgroundColor]}]};export{e as default};
